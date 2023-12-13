bc17dcc92fce1dbf55947acf7a713bf88aa5ca49
Keerthana D <103091347+Keerthana-13@users.noreply.github.com>
2023-12-13 16:41:20 +0530

    fruit_load_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,17 +45,20 @@ try:
 except URLError as e:
     streamlit.error()
 
-streamlit.write('The user entered ', fruit_choice)
+# Fruit Load Lsit
+streamlit.header("The Fruit Load list contains :")
+# Snowflake-related functions
+def get_fruit_load_list():
+     with my_cnx.cursor() as my_cur:
+          my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+          return my_cur.fetchall()
+# Add a button to load the fruit 
+if streamlit.button('Get Fruit Load List'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     my_data_rows = get_fruit_load_list()
+     streamlit.dataframe(my_data_rows)
+     
 streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The Fruit Load list contains : ")
-streamlit.dataframe(my_data_rows)
-
 
 fruit_choice1 = streamlit.text_input('What fruit would you like to add?')
 if fruit_choice1.strip():  # Check if the input is not empty or contains only spaces
